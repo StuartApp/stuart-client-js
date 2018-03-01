3cb78b936026b6063364de8a569ec982b1a321a2
Maximilien Tyc <maximilien.tyc@gmail.com>
2018-03-01 14:14:38 +0100

    refactor spec

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,7 +10,7 @@ const PackageJson = require('../package.json')
 
 const nock = require('nock')
 
-describe('Authenticator#getAccessToken', () => {
+describe('index', () => {
   beforeEach(() => {
     authenticator = new Authenticator(Environment.SANDBOX(), 'c6058849d0a056fc743203acb8e6a850dad103485c3edc51b16a9260cc7a7688', 'aa6a415fce31967501662c1960fcbfbf4745acff99acb19dbc1aae6f76c9c619')
 
@@ -25,90 +25,80 @@ describe('Authenticator#getAccessToken', () => {
       })
   })
 
-  describe('when no access token exists', () => {
-    test('returns a new access token', () => {
-      return authenticator.getAccessToken().then(
-        (accessToken) => expect(accessToken).toEqual('new-token')
-      )
+  describe('Authenticator#getAccessToken', () => {
+    describe('when no access token exists', () => {
+      test('returns a new access token', () => {
+        return authenticator.getAccessToken().then(
+          (accessToken) => expect(accessToken).toEqual('new-token')
+        )
+      })
+    })
+
+    describe('when a valid access token already exists', () => {
+      beforeEach(() => {
+        authenticator.accessToken = authenticator.oauthClient.createToken('current-valid-token', null, 'client_credentials', {})
+      })
+
+      it('returns the current valid access token', () => {
+        return authenticator.getAccessToken().then(
+          (accessToken) => expect(accessToken).toEqual('current-valid-token')
+        )
+      })
+    })
+
+    describe('when an expired access token already exists', () => {
+      beforeEach(() => {
+        authenticator.accessToken = authenticator.oauthClient.createToken('current-valid-token', null, 'client_credentials', {})
+        authenticator.accessToken.expired = jest.fn(() => {
+          return true
+        })
+      })
+
+      it('returns the current valid access token', () => {
+        return authenticator.getAccessToken().then(
+          (accessToken) => expect(accessToken).toEqual('new-token')
+        )
+      })
     })
   })
 
-  describe('when a valid access token already exists', () => {
+  describe('HttpClient#performGet', () => {
     beforeEach(() => {
-      authenticator.accessToken = authenticator.oauthClient.createToken('current-valid-token', null, 'client_credentials', {})
+      httpClient = new HttpClient(authenticator)
+
+      nock(httpClient.authenticator.environment.baseUrl)
+        .get('/some-url')
+        .reply(200, {
+          some: 'response'
+        })
+
+      nock(httpClient.authenticator.environment.baseUrl)
+        .post('/some-url')
+        .reply(200, {
+          some: 'response'
+        })
     })
 
-    it('returns the current valid access token', () => {
-      return authenticator.getAccessToken().then(
-        (accessToken) => expect(accessToken).toEqual('current-valid-token')
-      )
-    })
-  })
-
-  describe('when an expired access token already exists', () => {
-    beforeEach(() => {
-      authenticator.accessToken = authenticator.oauthClient.createToken('current-valid-token', null, 'client_credentials', {})
-      authenticator.accessToken.expired = jest.fn(() => {
-        return true
+    it('sends a get http request with correct parameters', () => {
+      const spy = jest.spyOn(Request, 'get')
+      return httpClient.performGet('/some-url').then(() => {
+        expect(spy).toHaveBeenCalledWith(
+          {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
+            'url': 'https://sandbox-api.stuart.com/some-url'},
+          expect.anything())
       })
     })
 
-    it('returns the current valid access token', () => {
-      return authenticator.getAccessToken().then(
-        (accessToken) => expect(accessToken).toEqual('new-token')
-      )
-    })
-  })
-})
-
-describe('HttpClient#performGet', () => {
-  beforeEach(() => {
-    authenticator = new Authenticator(Environment.SANDBOX(), 'c6058849d0a056fc743203acb8e6a850dad103485c3edc51b16a9260cc7a7688', 'aa6a415fce31967501662c1960fcbfbf4745acff99acb19dbc1aae6f76c9c619')
-
-    nock(authenticator.environment.baseUrl)
-      .post('/oauth/token')
-      .reply(200, {
-        access_token: 'new-token',
-        token_type: 'bearer',
-        expires_in: 2592000,
-        scope: 'api',
-        created_at: 1519753273
+    it('sends a post http request with correct parameters', () => {
+      const spy = jest.spyOn(Request, 'post')
+      return httpClient.performPost('/some-url', JSON.stringify({some: 'body'})).then(() => {
+        expect(spy).toHaveBeenCalledWith(
+          {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
+            'url': 'https://sandbox-api.stuart.com/some-url',
+            'body': '{"some":"body"}'},
+          expect.anything()
+        )
       })
-
-    httpClient = new HttpClient(authenticator)
-
-    nock(httpClient.authenticator.environment.baseUrl)
-      .get('/some-url')
-      .reply(200, {
-        some: 'response'
-      })
-
-    nock(httpClient.authenticator.environment.baseUrl)
-      .post('/some-url')
-      .reply(200, {
-        some: 'response'
-      })
-  })
-
-  it('sends a get http request with correct parameters', () => {
-    const spy = jest.spyOn(Request, 'get')
-    return httpClient.performGet('/some-url').then(() => {
-      expect(spy).toHaveBeenCalledWith(
-        {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
-          'url': 'https://sandbox-api.stuart.com/some-url'},
-        expect.anything())
-    })
-  })
-
-  it('sends a post http request with correct parameters', () => {
-    const spy = jest.spyOn(Request, 'post')
-    return httpClient.performPost('/some-url', JSON.stringify({some: 'body'})).then(() => {
-      expect(spy).toHaveBeenCalledWith(
-        {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
-          'url': 'https://sandbox-api.stuart.com/some-url',
-          'body': '{"some":"body"}'},
-        expect.anything()
-      )
     })
   })
 })
