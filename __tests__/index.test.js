e90323460d838e1013d3d7b5fd772720d86a5adf
Kasparas Masiukas <kmasiukas@gmail.com>
2018-06-20 10:43:06 +0200

    Added an optional parameter "params" to HttpClient method performGet. "params" object would allow passing query string parameters into the GET request.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -72,6 +72,12 @@ describe('index', () => {
         })
 
       nock(httpClient.authenticator.environment.baseUrl)
+        .get('/some-url?param1=one&param2=two')
+        .reply(200, {
+          some: 'response'
+        })
+
+      nock(httpClient.authenticator.environment.baseUrl)
         .post('/some-url')
         .reply(200, {
           some: 'response'
@@ -80,11 +86,25 @@ describe('index', () => {
 
     it('sends a get http request with correct parameters', () => {
       const spy = jest.spyOn(Request, 'get')
-      return httpClient.performGet('/some-url').then(() => {
+      return httpClient.performGet('/some-url').then((res) => {
         expect(spy).toHaveBeenCalledWith(
           {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
             'url': 'https://sandbox-api.stuart.com/some-url'},
           expect.anything())
+      })
+    })
+
+    it('sends a get http request with correct query string parameters', () => {
+      const spy = jest.spyOn(Request, 'get')
+      const params = {param1: 'one', param2: 'two'}
+      return httpClient.performGet('/some-url', params).then(() => {
+        expect(spy).toHaveBeenCalledWith(
+          {'headers': {'Authorization': 'Bearer new-token', 'Content-Type': 'application/json', 'User-Agent': 'stuart-client-js/' + PackageJson.version},
+            'url': 'https://sandbox-api.stuart.com/some-url',
+            'qs': {'param1': 'one', 'param2': 'two'}
+          },
+          expect.anything()
+        )
       })
     })
 
